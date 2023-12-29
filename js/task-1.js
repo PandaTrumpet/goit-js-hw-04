d46efb4a26bf3cfbde5184115aab61077cf0fc34
PandaTrumpet <dsavontrumpet@gmail.com>
2023-12-29 17:47:48 +0200

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,20 @@
 'use strict'
-function slugify(title) {
-    title = title.toLowerCase();
-   const newTitle = title.split(" ");
-   const result = newTitle.join("-");
-   return result;
+function isEnoughCapacity(products, containerSize) {
+products = Object.values(products)
+let totalProducts = 0;
+for(const element of products) {
+  totalProducts += element;
+} 
+if(totalProducts <= containerSize) {
+  return true;
+} else {
+  return false
+}
 }
 
-console.log(slugify("Arrays for begginers"));
-console.log(slugify("English for developer"));
-console.log(slugify("Ten secrets of JavaScript"));
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
+
+console.log( isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));//true
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));//false
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,27 @@
 'use strict'
-
-function filterArray(numbers, value) {
-    let newArray = [];
-    for(let number of numbers) {
-        if(number > value) {
-            newArray = newArray.concat(number);
-        } 
-    }
-    return newArray;
+const profile = {
+  username: "Jacob",
+playTime: 300,
+changeUsername(newName) {
+this.username = newName;
+},
+updatePlayTime(hours) {
+this.playTime = hours + this.playTime
+},
+getInfo() {
+return `${this.username} has ${this.playTime} active hours!`
 }
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); 
-console.log(filterArray([1, 2, 3, 4, 5], 4)); 
-console.log(filterArray([1, 2, 3, 4, 5], 5)); 
-console.log(filterArray([12, 24, 8, 41, 76], 38));
-console.log(filterArray([12, 24, 8, 41, 76], 20));
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+
